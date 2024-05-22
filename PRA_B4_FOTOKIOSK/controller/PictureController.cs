9cc6c13fb2fdd3f7a8584a9ccff0df0df529efcd
ph0en1x14 <phoenix20073101@gmail.com>
2024-05-22 09:47:47 +0200

    fotos van vaandag zien

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -3,9 +3,6 @@ using PRA_B4_FOTOKIOSK.models;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PRA_B4_FOTOKIOSK.controller
 {
@@ -14,43 +11,61 @@ namespace PRA_B4_FOTOKIOSK.controller
         // De window die we laten zien op het scherm
         public static Home Window { get; set; }
 
-
         // De lijst met fotos die we laten zien
         public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
-        
-        
+
+        // Map to hold directories for each day
+        private readonly Dictionary<DayOfWeek, string> dayDirectories = new Dictionary<DayOfWeek, string>
+        {
+            { DayOfWeek.Sunday, @"../../../fotos/0_Zondag" },
+            { DayOfWeek.Monday, @"../../../fotos/1_Maandag" },
+            { DayOfWeek.Tuesday, @"../../../fotos/2_Dinsdag" },
+            { DayOfWeek.Wednesday, @"../../../fotos/3_Woensdag" },
+            { DayOfWeek.Thursday, @"../../../fotos/4_Donderdag" },
+            { DayOfWeek.Friday, @"../../../fotos/5_vrijdag"},
+            { DayOfWeek.Saturday, @"../../../fotos/6_Zaterdag" }
+        };
+
         // Start methode die wordt aangeroepen wanneer de foto pagina opent.
         public void Start()
         {
+            var now = DateTime.Now;
+            var day = now.DayOfWeek;
 
             // Initializeer de lijst met fotos
-            // WAARSCHUWING. ZONDER FILTER LAADT DIT ALLES!
-            // foreach is een for-loop die door een array loopt
-            foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
+            if (dayDirectories.TryGetValue(day, out string directory))
             {
-                /**
-                 * dir string is de map waar de fotos in staan. Bijvoorbeeld:
-                 * \fotos\0_Zondag
-                 */
-                foreach (string file in Directory.GetFiles(dir))
-                {
-                    /**
-                     * file string is de file van de foto. Bijvoorbeeld:
-                     * \fotos\0_Zondag\10_05_30_id8824.jpg
-                     */
-                    PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
-                }
+                LoadPicturesFromDirectory(directory);
+            }
+            else
+            {
+                Console.WriteLine($"No directory assigned for {day}.");
             }
 
             // Update de fotos
             PictureManager.UpdatePictures(PicturesToDisplay);
         }
 
+        private void LoadPicturesFromDirectory(string rootDir)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(rootDir, "*", SearchOption.AllDirectories))
+                {
+                    PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle or log the error
+                Console.WriteLine($"Error loading pictures from directory '{rootDir}': {ex.Message}");
+            }
+        }
+
         // Wordt uitgevoerd wanneer er op de Refresh knop is geklikt
         public void RefreshButtonClick()
         {
-
+           
         }
-
     }
 }
