c3af6714d609ddcddeb48e8f377f99b5c9888535
D302620 <150166438+D302620@users.noreply.github.com>
2024-05-30 12:17:31 +0200

    B3 gemaakt
    bij zoeken wordt nu een de foto ingeladen die bij de ingevulde tijd hoort. de dag wordt vanzelf ingevuld met de huidige dag

--- PRA_B4_FOTOKIOSK/controller/SearchController.cs
+++ PRA_B4_FOTOKIOSK/controller/SearchController.cs
@@ -19,13 +19,58 @@ namespace PRA_B4_FOTOKIOSK.controller
         // very veel code smell "Django"
         public void Start()
         {
-
+            SearchManager.Instance = Window;
         }
 
         // Wordt uitgevoerd wanneer er op de Zoeken knop is geklikt
         public void SearchButtonClick()
         {
+            string picture;
+            string search = SearchManager.GetSearchInput();
+            var now = DateTime.Now;
+            int day = (int)now.DayOfWeek;
+            bool found = false;
 
+            foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
+            {
+                string[] parts = Path.GetFileName(dir).Split('_');
+                int foldersDays = int.Parse(parts[0]);
+
+                if (foldersDays == day)
+                {
+                    foreach (string file in Directory.GetFiles(dir))
+                    {
+                        string fileName = Path.GetFileNameWithoutExtension(file);
+                        string[] nameParts = fileName.Split('_');
+
+                        int fileHour = int.Parse(nameParts[0]);
+                        int fileMinute = int.Parse(nameParts[1]);
+                        int fileSecond = int.Parse(nameParts[2]);
+
+                        DateTime fileTime = new DateTime(now.Year, now.Month, now.Day, fileHour, fileMinute, fileSecond);
+                        string[] searchParts = search.Split("_");
+
+                        int searchHour = int.Parse(searchParts[0]);
+                        int searchMinute = int.Parse(searchParts[1]);
+                        int searchSecond = int.Parse(searchParts[2]);
+
+
+                        if (searchHour == fileHour && searchMinute == fileMinute && searchSecond == fileSecond)
+                        {
+                            picture = file;
+                            SearchManager.SetPicture(picture);
+                            found = true;
+                            break;
+                        }  
+                    }
+                    if (found == true)
+                    {
+                        break;
+                    }
+                }
+
+            }
+            
         }
 
     }
