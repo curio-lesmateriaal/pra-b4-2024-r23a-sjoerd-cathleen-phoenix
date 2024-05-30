deb3c783a5b1315a5944440567d3c428459226bd
ph0en1x14 <phoenix20073101@gmail.com>
2024-05-30 11:48:42 +0200

    A3 - redone
    moet miss later nog aanpassen.

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -22,6 +22,8 @@ namespace PRA_B4_FOTOKIOSK.controller
             DateTime lowerBound = now.AddMinutes(-30);
             DateTime upperBound = now.AddMinutes(-2);
 
+            List<KioskPhoto> camera1Photos = new List<KioskPhoto>();
+
             foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
             {
                 string[] parts = Path.GetFileName(dir).Split('_');
@@ -40,19 +42,49 @@ namespace PRA_B4_FOTOKIOSK.controller
 
                         DateTime fileTime = new DateTime(now.Year, now.Month, now.Day, fileHour, fileMinute, fileSecond);
 
-
                         if (fileTime >= lowerBound && fileTime <= upperBound)
                         {
-                            PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+                            camera1Photos.Add(new KioskPhoto() { Id = 0, Source = file });
                         }
                     }
                 }
             }
+
+            camera1Photos.Sort((x, y) => DateTime.Compare(GetDateTimeFromFileName(x.Source), GetDateTimeFromFileName(y.Source)));
+
+            List<KioskPhoto> camera2Photos = new List<KioskPhoto>();
+
+            foreach (var photo in camera1Photos)
+            {
+                DateTime photoTime = GetDateTimeFromFileName(photo.Source);
+                DateTime matchingTime = photoTime.AddSeconds(60);
+
+                var matchingPhoto = camera1Photos.FirstOrDefault(p => GetDateTimeFromFileName(p.Source) == matchingTime);
+
+                if (matchingPhoto != null)
+                {
+
+                    PicturesToDisplay.Add(photo);
+                    PicturesToDisplay.Add(matchingPhoto);
+                }
+            }
+
             PictureManager.UpdatePictures(PicturesToDisplay);
+        }
+
+
+        private DateTime GetDateTimeFromFileName(string fileName)
+        {
+            string[] nameParts = Path.GetFileNameWithoutExtension(fileName).Split('_');
+            int fileHour = int.Parse(nameParts[0]);
+            int fileMinute = int.Parse(nameParts[1]);
+            int fileSecond = int.Parse(nameParts[2]);
+            return new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, fileHour, fileMinute, fileSecond);
         }
 
         public void RefreshButtonClick()
         {
+
         }
     }
 }
